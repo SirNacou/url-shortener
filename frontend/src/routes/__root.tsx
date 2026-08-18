717bbe12b1a8845550f19d89cfac24b4b319a519
solus <quocanhthedev@gmail.com>
2026-08-18 20:01:11 +0700

    fix: reorder components in RootComponent for proper rendering

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -16,8 +16,8 @@ export const Route = createRootRoute({
 function RootComponent() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Outlet />
 			<Toaster />
+			<Outlet />
 			<TanStackDevtools
 				config={{
 					position: "bottom-right",
